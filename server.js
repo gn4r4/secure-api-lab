378edb4a7b5fb973f774bab8f589681a4120e0e2
gn4r <161369550+gn4r4@users.noreply.github.com>
2025-10-27 10:27:08 +0200

    feat: Complete final testing of the API

--- server.js
+++ server.js
@@ -67,9 +67,6 @@ const adminOnlyMiddleware = (req, res, next) => {
   next();
 };
 
-
-
-
 // --- МАРШРУТИ ДЛЯ РЕСУРСІВ --
 
 // Всі запити до /documents вимагають лише аутентифікації
@@ -78,8 +75,20 @@ app.get('/documents', authMiddleware, (req, res) => {
 });
 
 app.post('/documents', authMiddleware, (req, res) => {
-  const newDocument = req.body;
-  newDocument.id = Date.now();
+  const { title, content } = req.body;
+
+  // Перевірка, чи передані всі необхідні поля
+
+  if (!title || !content) {
+    return res.status(400).json({ message: 'Bad Request. Fields "title" and "content" are required.' });
+  }
+
+  const newDocument = {
+    id: Date.now(),
+    title,
+    content,
+  };
+
   documents.push(newDocument);
   res.status(201).json(newDocument);
 });
@@ -89,6 +98,23 @@ app.post('/documents', authMiddleware, (req, res) => {
 
 app.get('/employees', authMiddleware, adminOnlyMiddleware, (req, res) => {
   res.status(200).json(employees);
+});
+
+app.delete('/documents/:id', authMiddleware, (req, res) => {
+    // Отримуємо id з параметрів маршруту
+    const documentId = parseInt(req.params.id);
+    const documentIndex = documents.findIndex(doc => doc.id === documentId);
+
+    // Якщо документ з таким id не знайдено
+    if (documentIndex === -1) {
+        return res.status(404).json({ message: 'Document not found' });
+    }
+
+    // Видаляємо документ з масиву
+    documents.splice(documentIndex, 1);
+
+    // Відповідаємо статусом 204 No Content, тіло відповіді буде порожнім
+    res.status(204).send();
 });
 
 // --- КІНЕЦЬ МАРШРУТІВ ---
